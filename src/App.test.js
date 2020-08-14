8c6d12f7915ada977702a85af26de4a87245e2a9
mosesbenjamin <60680552+El-Mavewrick@users.noreply.github.com>
2020-08-15 00:22:16 +0100

    Jest Mock Function

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
-import { add, total } from './App'
+import { total } from './App'
+
+const add = jest.fn( ()=> 3);
 
 // Unit test
 // It only tests one thing
@@ -7,12 +9,15 @@ test("add", ()=>{
     // expect(value).toBe(3);
 
     expect(add (1,2)).toBe(3);
-    expect(add (5,2)).toBe(7);
+    // expect(add (1,2)).toBe(3);
+    expect(add).toHaveBeenCalledTimes(1);
+    expect(add).toHaveBeenCalledWith(1,2);
+    // expect(add (5,2)).toBe(7);
 });
 
 // Integration test
-
-test("total", ()=>{
-    expect(total(5, 20)).toBe("$25");
-});
+// Tests things working together
+// test("total", ()=>{
+//     expect(total(5, 20)).toBe("$25");
+// });
 
